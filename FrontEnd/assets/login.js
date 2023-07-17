24a832f38d459dd31f09629dc755d4021a333a00
Tristan Regueme <reguemedev@gmail.com>
2023-07-17 14:23:21 +0200

    latest version, fully functionnal (modified every single function)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,11 +1,15 @@
+//Function to submit the form and login in the website
 const loginSubmit = document.querySelector("#login form")
 loginSubmit.addEventListener("submit", async (event) => {
     event.preventDefault();
+    //Retrieving infos from the for and placing it in a const
     const loginInfos = {
         email: event.target.querySelector("[name=loginemail]").value,
         password: event.target.querySelector("[name=loginpassword]").value
     }
+    //Transforming login infos in json to use it
     const userLoginInfos = JSON.stringify(loginInfos)
+    //Sending form's information to login
     await fetch("http://localhost:5678/api/users/login", {
         method : 'POST',
         headers: {
@@ -13,7 +17,9 @@ loginSubmit.addEventListener("submit", async (event) => {
             "Content-Type": "application/json"
         },
         body: userLoginInfos,
-    }).then(async function(response) {
+    })
+    //If fetch is successfull stores the token from API and loads the homepage
+    .then(async function(response) {
         if (response.ok) {
             const res = await response.json()
             localStorage.setItem('token', `${res.token}`)
@@ -23,7 +29,3 @@ loginSubmit.addEventListener("submit", async (event) => {
         }
     })
 });
-/*
-email: sophie.bluel@test.tld
-password: S0phie
-*/
--- FrontEnd/assets/mainscript.js
+++ FrontEnd/assets/mainscript.js
@@ -3,6 +3,96 @@ const authToken = localStorage.getItem("token")
 const authString = `Bearer ${authToken}`
 console.log(authString)
 
+////////// GENERATING PROJECTS FROM API
+//Creating a global variable to place projects' datas
+let galleryContent = []
+
+// Function to fetch datas from API, place it the previous variable and generate gallery/modal
+async function displayingProjects() {
+    //Fetching datas from API
+    let response = await fetch('http://localhost:5678/api/works')
+    //Converting datas in json and placing it in a variable
+    galleryContent = await response.json()
+    generateMesProjets(galleryContent)
+    generateModal(galleryContent)
+}
+displayingProjects()
+
+// Function to generate the gallery
+function generateMesProjets(array) {
+    let gallery = document.querySelector(".gallery")
+    // Creating an html element "figure" for every element in the array
+    for (let i=0; i<array.length; i++) {
+        const galleryElement = document.createElement("figure")
+        //Creating an img element from the array
+        const galleryImage = document.createElement("img")
+        galleryImage.src = `${array[i].imageUrl}`
+        galleryImage.alt = `${array[i].title}`
+        galleryElement.appendChild(galleryImage)
+        //Creating figcaption element from the array
+        const galleryCaption = document.createElement("figcaption")
+        galleryCaption.innerHTML = `${array[i].title}`
+        galleryElement.appendChild(galleryCaption)
+        
+        gallery.appendChild(galleryElement)
+    }
+}
+
+// Function to generate the modal
+function generateModal(array) {
+    let modal = document.querySelector("#modal-gallery")
+    for (let i=0; i<array.length; i++) {
+        // Creating a division for every project in the array
+        const divModalImage = document.createElement("div")
+
+        // Creating modal's gallery from 
+        const modalImage = document.createElement("img")
+        modalImage.src = `${array[i].imageUrl}`
+        modalImage.alt = `${array[i].title}`
+        divModalImage.appendChild(modalImage)
+
+        // Creating Button to delete projects
+        const btnDeleteWork = document.createElement("i")
+        btnDeleteWork.classList.add("fa-trash-can","fa-solid","fa-lg")
+        btnDeleteWork.id = `${array[i].id}`
+        divModalImage.appendChild(btnDeleteWork)
+        btnDeleteWork.addEventListener("click", () => {
+            deleteWork(btnDeleteWork.id)
+        })
+
+        // Creating fake button to move work
+        const btnMoveWork = document.createElement("i")
+        btnMoveWork.classList.add("fa-arrows-up-down-left-right","fa-solid","fa-lg")
+        divModalImage.appendChild(btnMoveWork)
+
+        // Creating fake button to edit
+        const modalEdit = document.createElement("a")
+        modalEdit.innerHTML = "éditer"
+        divModalImage.appendChild(modalEdit)
+
+        modal.appendChild(divModalImage)
+    }
+}
+
+////////// Function to delete a project of the website
+async function deleteWork(id) {
+    let response = await fetch(`http://localhost:5678/api/works/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Authorization": authString
+        }
+    })
+    if (response.ok) {
+        // reset works in gallery & modal
+        document.querySelector(".gallery").innerHTML = ""
+        document.querySelector("#modal-gallery").innerHTML = ""
+        displayingProjects()
+    } else {
+        window.alert(`Erreur lors de la suppression du projet`)
+    }
+}
+
+/* Functions to fetch API and generate modal/gallery
 // Fetching Gallery's element from API
 const reponse = await fetch('http://localhost:5678/api/works')
 let galleryContent = await reponse.json()
@@ -29,171 +119,6 @@ function generateMesProjets(array, target) {
 }
 const projets = document.querySelector(".gallery")
 generateMesProjets(galleryContent, projets)
-
-// Displaying used filter
-let selected_btn = document.getElementsByClassName("btn")[0]
-selected_btn.classList.add("selected_button")
-function changeSelectedBtn(id) {
-    selected_btn.classList.remove("selected_button")
-    selected_btn = document.getElementsByClassName("btn")[id]
-    selected_btn.classList.add("selected_button")
-}
-
-// Function to filter elements by categories with filters
-function filterWorks(id) {
-    changeSelectedBtn(id)
-    if (id === 0) {
-        projets.innerHTML = '';
-        generateMesProjets(galleryContent,projets)
-    } else {
-        let filterContent = []
-        for (let i=0; i<galleryContent.length; i++) {
-            if (galleryContent[i].categoryId === id) {
-                filterContent.push(galleryContent[i])
-            }
-        }
-        projets.innerHTML = '';
-        generateMesProjets(filterContent,projets);
-    }
-}
-
-//Event Listeners for each filter button
-const buttonTous = document.getElementsByClassName("btn")[0]
-buttonTous.addEventListener('click', () => {
-    filterWorks(0)
-})
-const buttonObj = document.getElementsByClassName("btn")[1]
-buttonObj.addEventListener('click', () => {
-    filterWorks(1)
-})
-const buttonAppt = document.getElementsByClassName("btn")[2]
-buttonAppt.addEventListener('click', () => {
-    filterWorks(2)
-})
-const buttonHetr = document.getElementsByClassName("btn")[3]
-buttonHetr.addEventListener('click', () => {
-    filterWorks(3)
-})
-
-//Function to show admin interface
-function showAdminInterface() {
-    document.querySelector("body > div").classList.remove("display_none")
-    document.querySelector("#introduction figure a").classList.remove("display_none")
-    document.querySelector("#introduction article a").classList.remove("display_none")
-    document.querySelector("#projets a").classList.remove("display_none")
-    document.querySelector(".filters").classList.add("display_none")
-}
-if (authToken !== null) {
-    showAdminInterface()
-}
-
-// Declaring variables to keep focus in modals while using keyboard
-const focussableSelectors = "button, a, input, textarea"
-let focussables = []
-let previouslyFocussedElement = null
-
-// Function to open a modal
-const openModal = function (e, target) {
-    e.preventDefault()
-    console.log("hello")
-    let modal = document.querySelector(target)
-    focussables = Array.from(modal.querySelectorAll(focussableSelectors))
-    previouslyFocussedElement = document.querySelector(":focus")
-    modal.classList.remove("display_none")
-    modal.classList.add("modal")
-    focussables[0].focus()
-    modal.removeAttribute("aria-hidden")
-    modal.setAttribute("aria-modal", true)
-    modal.addEventListener("click", () => {
-        closeModal(e, target)
-    })
-}
-
-// Function to close a modal
-const closeModal = function (e, target) {
-    let modal = document.querySelector(target)
-    if (previouslyFocussedElement !== null) previouslyFocussedElement.focus()
-    e.preventDefault()
-    window.setTimeout(function () {
-        modal.classList.add("display_none")
-        modal.classList.remove("modal")
-        modal = null
-    }, 500)
-    modal.removeAttribute("aria-modal")
-    modal.setAttribute("aria-hidden", true)
-    modal.removeEventListener("click", () => {
-        closeModal(e, target)
-    })
-}
-
-//Function to keep the focus in modal if user is using keyboard
-const focusInModal = function (e) {
-    e.preventDefault()
-    let index = focussables.findIndex(f => f === modal.querySelector(":focus"))
-    if(e.shiftKey === true) {
-        index--
-    } else {
-        index++
-    }
-    if (index >= focussables.length) {
-        index = 0
-    }
-    if (index < 0) {
-        index = focussables.length -1
-    }
-    focussables[index].focus()
-}
-
-//Stop the propagation on the event
-const stopPropagation = function (e) {
-    e.stopPropagation()
-}
-
-document.querySelectorAll(".js-modal").forEach(a => {
-    a.addEventListener("click", function (e) {
-        openModal(e, "#modal-admin")
-    })
-})
-
-/*
-window.addEventListener("keydown", function (e, target) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeGalleryModal(e)
-    }
-    if (e.key === "Tab" & modal !== null) {
-        focusInModal(e)
-    }
-})
-*/
-
-//Event listeners to stop propagation of modals
-document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
-document.querySelector(".js-modal-stop2").addEventListener("click", stopPropagation)
-
-// Event listeners to open modal-admin & modal-add-project
-document.querySelector("#addPhoto").addEventListener("click", function (e, target) {
-    closeModal(e, "#modal-admin")
-})
-document.querySelector("#addPhoto").addEventListener("click", function (e, target) {
-    openModal(e, "#modal-add-project")
-})
-
-//Event listeners to close modal-admin & modal-add-project
-document.querySelector(".js-modal-close").addEventListener("click", function (e, target) {
-    closeModal(e, "#modal-admin")
-})
-document.querySelector(".js-modal-close2").addEventListener("click", function (e, target) {
-    closeModal(e, "#modal-add-project")
-})
-
-//Event listeners to return to modal-admin & reset modal-add-project  
-document.querySelector(".js-previous-modal").addEventListener("click", (e) => {
-    openModal(e, "#modal-admin")
-})
-document.querySelector(".js-previous-modal").addEventListener("click", function (e, target) {
-    closeModal(e, "#modal-add-project")
-    document.getElementById("error-msg").innerHTML= ""
-})
 
 // Display gallery in modal
 const modalGallery = document.querySelector("#modal-gallery")
@@ -233,40 +158,166 @@ function generateModal(array, target) {
         }
     }
 generateModal(galleryContent, modalGallery)
+*/
 
-function deleteWork(id) {
-    fetch(`http://localhost:5678/api/works/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": authString
-        }
-    })
-    .then((res) => res.json())/*
-        if (response.ok) {
-            // reset works in gallery & modal
-            projets.innerHTML = ""
-            modalGallery.innerHTML = ""
-            generateMesProjets(galleryContent, projets)
-            generateModal(galleryContent, modalGallery)
-            // update gallery content
-            /*
-            async () => {
-                const request = await fetch('http://localhost:5678/api/works')
-                galleryContent = await request.json()
-                console.log(galleryContent)
-                displays works in gallery & modal
-                generateMesProjets(galleryContent, projets)
-                generateModal(galleryContent, modalGallery)
-            }
-            
-        } else {
-            window.alert(`Erreur lors de la suppression du projet`)
-        }
-    })*/
+////////// FILTERS
+// Displaying used filter
+let selected_btn = document.getElementsByClassName("btn")[0]
+selected_btn.classList.add("selected_button")
+function changeSelectedBtn(id) {
+    selected_btn.classList.remove("selected_button")
+    selected_btn = document.getElementsByClassName("btn")[id]
+    selected_btn.classList.add("selected_button")
 }
 
+// Function to filter elements by categories with filters
+function filterWorks(id) {
+    changeSelectedBtn(id)
+    if (id === 0) {
+        document.querySelector(".gallery").innerHTML = ""
+        generateMesProjets(galleryContent)
+    } else {
+        //Creating an new array containing elements by ids
+        let filterContent = []
+        for (let i=0; i<galleryContent.length; i++) {
+            if (galleryContent[i].categoryId === id) {
+                filterContent.push(galleryContent[i])
+            }
+        }
+        document.querySelector(".gallery").innerHTML = ""
+        generateMesProjets(filterContent)
+    }
+}
 
-// display uploaded image
+//Event Listeners for each filter button
+const buttonTous = document.getElementsByClassName("btn")[0]
+buttonTous.addEventListener('click', () => {
+    filterWorks(0)
+})
+const buttonObj = document.getElementsByClassName("btn")[1]
+buttonObj.addEventListener('click', () => {
+    filterWorks(1)
+})
+const buttonAppt = document.getElementsByClassName("btn")[2]
+buttonAppt.addEventListener('click', () => {
+    filterWorks(2)
+})
+const buttonHetr = document.getElementsByClassName("btn")[3]
+buttonHetr.addEventListener('click', () => {
+    filterWorks(3)
+})
+
+////////// ADMIN INTERFACE
+//Shows admin interface if admin token is detected
+function showAdminInterface() {
+    document.querySelector("body > div").classList.remove("display_none")
+    document.querySelector("#introduction figure a").classList.remove("display_none")
+    document.querySelector("#introduction article a").classList.remove("display_none")
+    document.querySelector("#projets a").classList.remove("display_none")
+    document.querySelector("#logout").classList.remove("display_none")
+    document.querySelector(".filters").classList.add("display_none")
+}
+if (authToken !== null) {
+    showAdminInterface()
+}
+
+//Resets local storage and reload url
+document.querySelector("#logout").addEventListener("click", () => {
+    localStorage.clear()
+    location.reload()
+})
+
+///////////// MODALS
+// Declaring variables to keep focus in modals while using keyboard
+const focussableSelectors = "button, a, input, textarea"
+let focussables = []
+let previouslyFocussedElement = null
+
+// Function to open a modal
+const openModal = function (e, target) {
+    e.preventDefault()
+    let modal = document.querySelector(target)
+    focussables = Array.from(modal.querySelectorAll(focussableSelectors))
+    previouslyFocussedElement = document.querySelector(":focus")
+    modal.classList.remove("display_none")
+    modal.classList.add("modal")
+    focussables[0].focus()
+    modal.removeAttribute("aria-hidden")
+    modal.setAttribute("aria-modal", true)
+    modal.addEventListener("click", () => {
+        closeModal(e, target)
+    })
+}
+
+// Function to close a modal
+const closeModal = function (e, target) {
+    let modal = document.querySelector(target)
+    if (previouslyFocussedElement !== null) previouslyFocussedElement.focus()
+    e.preventDefault()
+    window.setTimeout(function () {
+        modal.classList.add("display_none")
+        modal.classList.remove("modal")
+        modal = null
+    }, 500)
+    modal.removeAttribute("aria-modal")
+    modal.setAttribute("aria-hidden", true)
+    modal.removeEventListener("click", () => {
+        closeModal(e, target)
+    })
+}
+
+//Stop the propagation on the event
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+//Placing an event listener on each "modifier" button
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", function (e) {
+        openModal(e, "#modal-admin")
+    })
+})
+
+//Placing an event listener to close modals with escape
+window.addEventListener("keydown", function (e, target) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e, "#modal-admin")
+        closeModal(e, "#modal-add-project")
+    }
+})
+
+////////// EVENT LISTENERS FOR MODALS
+//Event listeners to stop propagation of modals
+document.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+document.querySelector(".js-modal-stop2").addEventListener("click", stopPropagation)
+
+// Event listeners to open modal-admin & modal-add-project
+document.querySelector("#addPhoto").addEventListener("click", function (e, target) {
+    closeModal(e, "#modal-admin")
+})
+document.querySelector("#addPhoto").addEventListener("click", function (e, target) {
+    openModal(e, "#modal-add-project")
+})
+
+//Event listeners to close modal-admin & modal-add-project
+document.querySelector(".js-modal-close").addEventListener("click", function (e, target) {
+    closeModal(e, "#modal-admin")
+})
+document.querySelector(".js-modal-close2").addEventListener("click", function (e, target) {
+    closeModal(e, "#modal-add-project")
+})
+
+//Event listeners to return to modal-admin & reset modal-add-project  
+document.querySelector(".js-previous-modal").addEventListener("click", function (e, target) {
+    openModal(e, "#modal-admin")
+})
+document.querySelector(".js-previous-modal").addEventListener("click", function (e, target) {
+    closeModal(e, "#modal-add-project")
+    document.getElementById("error-msg").innerHTML= ""
+})
+
+////////// MODAL TO ADD A PROJECT TO THE WEBSITE
+// display the uploaded image in the modal
 document.querySelector("#new-img").addEventListener("change", () => {
     let file = document.getElementById("new-img").files[0]
     console.log(file.size)
@@ -294,7 +345,7 @@ function removeUploadedImg() {
     document.querySelector(".upload-img").classList.remove("display_none")
 }
 
-////////// ADD A PROJECT TO THE WEBSITE
+////////// FUNCTION TO ADD A PROJECT TO THE WEBSITE
 document.getElementById("add-project").addEventListener("submit", (e) => {
     e.preventDefault()
 
@@ -326,7 +377,6 @@ document.getElementById("add-project").addEventListener("submit", (e) => {
                         //Retrieving photo&title 
                         let image = document.getElementById("new-img").files[0]
                         let imagetitle = document.getElementById("new-title").value;
-                        // authString
 
                         //Checking photo's weight (has to be <4mo)
                         if (image.size < 4 * 1000001) { 
@@ -347,10 +397,10 @@ document.getElementById("add-project").addEventListener("submit", (e) => {
                                 })
                                 //Checking request's status & displaying galleries
                                 if (request.status === 201) {
-                                    projets.innerHTML = ""
-                                    modalGallery.innerHTML = ""
-                                    generateMesProjets(galleryContent, projets)
-                                    generateModal(galleryContent, modalGallery)
+                                    // reset works in gallery & modal
+                                    document.querySelector(".gallery").innerHTML = ""
+                                    document.querySelector("#modal-gallery").innerHTML = ""
+                                    displayingProjects()
                                 } else {
                                     window.alert(`Un problème est survenu lors de l'ajout du projet`)
                                 }
